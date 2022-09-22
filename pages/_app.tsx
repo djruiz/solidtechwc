3a702098050080fa2644ad1bd8046982332e64f5
Daniel Ruiz <djruiz@umail.iu.edu>
2022-09-22 18:21:10 -0400

    First fold designed

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,11 @@
-import type { AppProps } from 'next/app'
-import "bootstrap/dist/css/bootstrap.min.css"
-import "../styles/global.css"
-import { useUnderConstruction } from '../hooks/useUnderConstruction'
+import type { AppProps } from "next/app";
+import "../styles/bootstrap.min.css";
+import "../styles/global.css";
+import { useUnderConstruction } from "../hooks/useUnderConstruction";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  const { showSite, underConstructionMessage } = useUnderConstruction()
-  if (!showSite) return underConstructionMessage
+  const { showSite, underConstructionMessage } = useUnderConstruction();
+  if (!showSite) return underConstructionMessage;
 
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -6,12 +6,18 @@ import Tossed from "../img/tossed.jpg";
 interface Props {
   children: React.ReactNode;
   color: string;
+  flip?: boolean;
 }
 
 const Header = (props: Props) => {
   return (
-    <div className="d-flex align-items-center">
-      <span style={{ color: props.color }}>{props.children}</span>
+    <div
+      style={{ flexDirection: props.flip ? "row-reverse" : "row" }}
+      className="d-flex align-items-center"
+    >
+      <span className="d-flex" style={{ color: props.color, flexShrink: 0 }}>
+        {props.children}
+      </span>
       <div
         style={{ width: "100%", height: "3px", backgroundColor: props.color }}
       ></div>
@@ -21,20 +27,30 @@ const Header = (props: Props) => {
 
 export default () => {
   return (
-    <div className="bg-light mh-100" style={{ paddingTop: "50px" }}>
-      <div className="bg-dark container">
-        <Header color="#ff0505">
-          <h1 style={{ minWidth: "5em" }}>Solid Tech</h1>
-        </Header>
+    <div className="bg-dark mh-100" style={{ paddingTop: "50px" }}>
+      <div className="bg-dark container" style={{ maxWidth: "750px" }}>
         <Header color="#fff">
-          <h1 style={{ minWidth: "8em" }}>Wrestling Club</h1>
-          <Image src={Lightning} />
+          <h1 className="mr-4 open-sans">Solid Tech</h1>
         </Header>
-        <Image src={Tossed} />
-      </div>
-      <div className="d-flex">
-        <h1></h1>
-        <div></div>
+        <Header color="#ff0505">
+          <div className="d-flex align-items-center" style={{ width: "100%" }}>
+            <h1 className="exo">Wrestling Club</h1>
+            <Image width={72.5} height={55.5} src={Lightning} />
+          </div>
+        </Header>
+        <div className="d-flex justify-content-end my-4">
+          <div
+            className="rounded overflow-hidden shadow"
+            style={{ maxWidth: 650, width: "100%" }}
+          >
+            <Image layout="responsive" src={Tossed} />
+          </div>
+        </div>
+        <Header color="#fff" flip>
+          <h2 className="ml-4 londrina">
+            Stay. <span className="text-primary">Solid.</span>
+          </h2>
+        </Header>
       </div>
     </div>
   );
