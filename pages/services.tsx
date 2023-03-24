a3fb3572190c64866f3e2b60eabf13f1439c1fb0
djruiz <djruiz@umail.iu.edu>
2023-03-24 13:04:24 -0400

    Add stripe payments

---
+++ pages/services.tsx
@@ -0,0 +1,31 @@
+import Link from "next/link"
+
+export default () => {
+    return  <div className="container text-center mt-4">
+        <h1>
+            Services
+        </h1>
+        <div className="service" id="drop-in">
+            <Link href="https://buy.stripe.com/8wM3e40pgg0S3m028b">
+                <button className="mt-4 position-relative btn btn-primary btn-lg w-100 shadow-med exo">Drop-In Class</button>
+            </Link>
+        </div>
+        <div className="service" id="drop-in">
+            <Link href="https://buy.stripe.com/5kA5mc4Fw7um3m06os">
+                <button className="mt-4  position-relative btn btn-primary btn-lg w-100 shadow-med exo">Order a Singlet</button>
+            </Link>
+        </div>
+
+        <div className="service" id="drop-in">
+            <Link href="https://buy.stripe.com/28o3e4b3UdSK09O5kq">
+                <button className="mt-4 position-relative btn btn-primary btn-lg w-100 shadow-med exo">Virginia Tournament</button>
+            </Link>
+        </div>
+
+        <div className="service" id="drop-in">
+            <Link href="https://buy.stripe.com/7sIg0Q8VMbKCe0EcMR">
+                <button className="mt-4 position-relative btn btn-primary btn-lg w-100 shadow-med exo">Virginia Tournament with transportation and Airbnb</button>
+            </Link>
+        </div>
+    </div>   
+}
